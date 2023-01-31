87da26236db31916e1700cf525fd674ac8fcf42f
Bashar-Saad-95 <basharsaad697@gmail.com>
2023-01-31 12:00:21 +0200

    Uplaod The Project Final

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(function () {
+    'Use Strict'
+    $(window).scroll(function () {
+        var navbar = $('.navbar');
+        if ($(window).scrollTop() >= navbar.height()) {
+            navbar.addClass('scrolled');
+        } else 
+        {
+            navbar.removeClass('scrolled');
+        }
+    });
+    // Deal With Tabs
+    $('.tab-switch li').click(function () {
+        //Add Selected Class To Active Link
+        $(this).addClass('selected').siblings().removeClass('selected');
+        //Hide All Divs
+        $('.tabs-section .tabs-content > div').hide();
+        //Show Div Connected With This Link
+        $($(this).date('class')).show();
+    });
+});
+    
